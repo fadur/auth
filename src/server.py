610f02bc05926fa3e503f9903712336b3d635dc5
Feisal Adur <iam@feisal.dk>
2022-09-27 12:55:53 +0200

    fixes

--- src/server.py
+++ src/server.py
@@ -1,39 +1,72 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
 from aiohttp import web
+import aiohttp_cors
 
 from register import register_cognito_user, confirm_cognito_user
 from auth import login, refresh_token, authorize
 from user_attributes import user_attributes
 
+logging.basicConfig(level=logging.DEBUG)
+
 
 async def index(request):
-    username = request.query.get("username")
-    password = request.query.get("password")
-    response = await register_cognito_user(username, password)
+    body = await request.json()
+    password = body.get("password")
+    email = body.get("email")
+    username = email
+    try:
+        response = await register_cognito_user(username, password)
+    except Exception as e:
+        erro_msg = str(e)
+        error = erro_msg.split(":")[1:]
+        msg = "\n".join(error)
+        response = {"error": msg}
+        return web.json_response(response, status=500)
     return web.json_response(response)
 
 
 async def confirm(request):
-    username = request.query.get("username")
-    code = request.query.get("code")
-    response = await confirm_cognito_user(username, code)
+    body = await request.json()
+    username = body.get("username")
+    code = body.get("code")
+    try:
+        response = await confirm_cognito_user(username, code)
+    except Exception as e:
+        response = {"error": str(e)}
     return web.json_response(response)
 
 
 async def sign_in(request):
     body = await request.json()
     username = body.get("username")
+    if not username:
+        username = body.get("email")
     password = body.get("password")
-    response = await login(username, password)
+    try:
+        response = await login(username, password)
+    except Exception as e:
+        erro_msg = str(e)
+        error = erro_msg.split(":")[-1]
+        response = {"error": error}
+        return web.json_response(response, status=500)
     return web.json_response(response)
 
 
 async def refresh(request):
-    username = request.query.get("username")
-    token = request.query.get("refresh_token")
-    print(token)
-    response = await refresh_token(username, token)
+    body = await request.json()
+    token = body.get("token")
+    username = body.get("username")
+
+    if not username:
+        username = body.get("email")
+
+    try:
+        response = await refresh_token(username, token)
+    except Exception as e:
+        response = {"error": str(e)}
+        return web.json_response(response, status=500)
     return web.json_response(response)
 
 
@@ -49,10 +82,28 @@ async def user(request):
 
 
 app = web.Application()
-app.router.add_route("GET", "/", index)
-app.router.add_route("GET", "/confirm", confirm)
+cors = aiohttp_cors.setup(
+    app,
+    defaults={
+        "*": aiohttp_cors.ResourceOptions(
+            allow_credentials=True,
+            expose_headers="*",
+            allow_headers="*",
+        )
+    },
+)
+
+# secure cookie session
+# session_middleware = aiohttp_session.cookie_storage.EncryptedCookieStorage(
+#     secret_key=os.urandom(32)
+#
+
+
+cors.add(app.router.add_post("/register", index))
+cors.add(app.router.add_post("/confirm", confirm))
+
 app.router.add_route("POST", "/login", sign_in)
-app.router.add_route("GET", "/refresh", refresh)
+app.router.add_route("POST", "/refresh", refresh)
 app.router.add_route("GET", "/auth", auth)
-app.router.add_route("POST", "/user", user)
+app.router.add_route("POST", "/update-profile", user)
 web.run_app(app)
--- src/user_attributes.py
+++ src/user_attributes.py
@@ -3,27 +3,22 @@
 import boto3
 import os
 
-ClientId = os.environ.get('CLIENT_ID')
+ClientId = os.environ.get("CLIENT_ID")
 
 
 async def user_attributes(request):
     body = await request.json()
-    name = body.get('name')
-    phone_number = body.get('phone_number')
-    access_token = request.headers.get('Authorization')
+    name = body.get("name")
+    username = body.get("username")
+    # phone_number = body.get("phone_number")
+    access_token = request.headers.get("Authorization")
     print(access_token)
-    client = boto3.client('cognito-idp')
+    client = boto3.client("cognito-idp")
     response = client.update_user_attributes(
-        AccessToken=access_token.split(' ')[1],
+        AccessToken=access_token.split(" ")[1],
         UserAttributes=[
-            {
-                'Name': 'name',
-                'Value': name
-            },
-            {
-                'Name': 'phone_number',
-                'Value': phone_number
-            },
+            {"Name": "name", "Value": name},
+            {"Name": "username", "Value": username},
         ],
     )
     return response
